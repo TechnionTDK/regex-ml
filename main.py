075efd1b9ae42f257bbcdc3c6a1a1f0295381b95
shakedmeitav <38721178+shakedmeitav@users.noreply.github.com>
2019-12-24 14:58:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,9 @@ from snorkel.labeling import MajorityLabelVoter
 
 from snorkel.labeling import PandasLFApplier
 
+from snorkel.labeling import LFAnalysis
+
+from snorkel.analysis import get_label_buckets
 
 # import snorkel.labeling
 #@labeling_function()
@@ -25,24 +28,30 @@ ABSTAIN = -1
 REF = 1
 
 #not tag
-UNTAG = 0
+NO_REF = 0
 "maybe we have a rejection function"
 
-SAMPLE_SIZE = 2
+SAMPLE_SIZE = 100
 K_GRAM = 6
+
 def load_torat_emet_data(): #TODO:in example it is in utils, in our case it is here, so transfer func
-    whole_text = csv_to_string()
-    #TODO: avoid creating sample that is half end of line k and half beginning of line k+1
-    k_gram_series = pd.Series(generate_ngrams(whole_text, K_GRAM))
-    data_frame = pd.DataFrame({'text':k_gram_series})
+    df = pd.read_csv('csvRes.csv')
+    data = ''
+    k_gram_series = pd.Series()
+
+    for x in range(SAMPLE_SIZE):
+        data = df['text'][x]
+        k_gram_series_for_one = pd.Series(generate_ngrams(data, K_GRAM))
+        k_gram_series= k_gram_series.append(k_gram_series_for_one, ignore_index=True)
+    #print(k_gram_series)
+   # k_gram_series = pd.Series(generate_ngrams(whole_text, K_GRAM))
+
+    data_frame = pd.DataFrame({'text': k_gram_series})
+    #print(data_frame.size)
     data_frame['tag'] = ABSTAIN
     training_set, test_set = train_test_split(data_frame, test_size=0.2)
-    #print(training_set)
     return training_set, test_set
     #print(data_frame) #now we have untagged df
-
-
-
 
 
 def csv_to_string():
@@ -70,10 +79,6 @@ def generate_ngrams(s, n):
     # Concatentate the tokens into ngrams and return
     ngrams = zip(*[tokens[i:] for i in range(n)])
     return [" ".join(ngram) for ngram in ngrams]
-
-
-
-
 
 
 
@@ -162,23 +167,45 @@ def run_lf_on_data():
     label_model = LabelModel(cardinality=2, verbose=True)
     label_model.fit(L_train=l_train, n_epochs=500, lr=0.001, log_freq=100, seed=123)
     '''
+    print("=======")
+    print(LFAnalysis(L=l_train, lfs=lfs).lf_summary())
 
     majority_model = MajorityLabelVoter()
     preds_train = majority_model.predict(L=l_train)
-    print(preds_train)
+
+    #df_fn_train = df_train[["text", "label"]].iloc[buckets[(REF, NO_REF)]]
+    #print(df_fn_train.sample(5, random_state=3))
+    #print(preds_train)
 
     for x in range(preds_train.size):
         df_train['tag'][x]=preds_train[x]
 
-    print(df_train)
+    print("=======================")
+   # print(df_train)
+    df_train.to_csv(r'C:\private\Shaked\Technion\shaked_technion\winter 2019-2020\project_new\file.csv', index=False)
+
+
 
 #אנו רוצים שהדאטאפריים שלנו יכיל 2 עמודות text label
 #Y_dev = df_dev.label.values
 #Y_valid = df_valid.label.values
 
 
-
-
+def create_devset(train_set):
+    dev = pd.DataFrame(columns=('text', 'tag'))
+    counter = 0
+    print("the size is : ", train_set.size)
+    for index, row in train_set.iterrows():
+        print(row['text'])
+        ans = input("please tag : 1 is tag and 0 is untag")
+        new_row = {'text': row['text'], 'tag': ans}
+        dev = dev.append(new_row, ignore_index=True)
+        if ans is '1':
+            counter = counter + 1
+        if counter == 50:
+            break
+    dev.to_csv(r'C:\private\Shaked\Technion\shaked_technion\winter 2019-2020\project_new\dev.csv', index=False)
+    return dev
 
 
 def main():
@@ -187,6 +214,8 @@ def main():
     # new = generate_ngrams(s, 6)
     #print((new))
     run_lf_on_data()
+    #df_train, df_test = load_torat_emet_data()
+    #dev = create_devset(df_train)
 
 
 if __name__ == "__main__":
